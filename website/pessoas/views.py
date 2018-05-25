af62425cfd22b3398ac8a2cf065158ad28f6dd15
unknown <paulo.es@live.com>
2018-05-25 16:22:29 -0300

    Creating view for Pessoa
    Creating methods to return list or a single one and to create, update or delete an instance

--- website/pessoas/views.py
+++ website/pessoas/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from pessoas.models import Pessoa
+from pessoas.serializers import PessoaSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class PessoaList(APIView):
+    """
+    List all pessoas, or create a new pessoa
+    """
+    def get(self, request, format=None):
+        pessoas = Pessoa.objects.all()
+        serializer = PessoaSerializer(pessoas, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+class PessoaDetail(APIView):
+    """
+    Retrieve, update or delete a pessoas instance
+    """
+    def get_object(self, pk):
+        try:
+            return Pessoa.objects.get(pk=pk)
+        except Pessoa.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        pessoa = self.get_object(pk)
+        serializer = PessoaSerializer(pessoa, data=request.data)
+        if serializer.is_valid():
+            serializer.save() 
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk, format=None):
+        pessoa = self.get_object(pk)
+        serializer = PessoaSerializer(pessoa, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk, format=None):
+        pessoa = self.get_object(pk)
+        snippet.delefe()
+        return Response(status=status.HTTP_204)
