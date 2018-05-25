b606331b6f6aec719db6ac062c9ed354f3e7e820
unknown <paulo.es@live.com>
2018-05-25 16:45:43 -0300

    Fixing some miss typing

--- website/pessoas/views.py
+++ website/pessoas/views.py
@@ -16,11 +16,11 @@ class PessoaList(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
+        serializer = PessoaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PessoaDetail(APIView):
     """
