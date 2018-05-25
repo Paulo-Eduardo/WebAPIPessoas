6e06ac03b76743fe1d7a3c3fe48ed696039dfacd
unknown <paulo.es@live.com>
2018-05-25 15:36:26 -0300

    Creating new model for Pessoa

--- website/pessoas/models.py
+++ website/pessoas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+GENDERS = (
+    ('M', "Male"),
+    ('F', "Female"),
+    ('R', "Rather not say"),
+)
+
 # Create your models here.
+class Pessoa(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    gender = models.CharField(choices=GENDERS, default='R', max_length=10)
+    age = models.IntegerField()
+
+
+    class Meta:
+        ordering = ('id',)
